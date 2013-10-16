19486b192e10f3dfdc6cc7a72057e88d00c2c329
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2013-10-17 08:27:11 +1000

    Made Component object.

--- demo/tmp/demo.js
+++ demo/tmp/demo.js
@@ -2,31 +2,50 @@
 var app = angular.module("JohnnyFiveDemo", []);
 window.app = app;
 
+function Component(type, pin) {
+	
+	this.type = type ? type : Component.prototype.types[0];
+
+	if (pin) {
+		this.pin = pin;		
+	} else {
+		this.pin = Component.prototype.lastPin++;		
+	}
+}
+
+Component.prototype = {
+	constructor: Component,
+	types : [
+		{name: 'Led', value: 'Led'},
+		{name: 'Servo', value: 'Servo'},
+		{name: 'Button', value: 'Button'},
+	],
+
+	type : null,
+	pin : 0,
+	lastPin : 13,
+
+
+	isLed : function() {	return this.type.value == "Led"; 		},
+	isServo : function() {	return this.type == "Servo"; 	},
+	isButton : function() {	return this.type == "Button";	},
+};
+
 app.controller("DemoCtrl", function($scope) {
 	window.$scope = $scope;
 
 	$scope.data = {
-		componentTypes: [
-			{name: 'Led', value: 'Led'},
-			{name: 'Servo', value: 'Servo'},
-			{name: 'Button', value: 'Button'},
-		]
+		componentTypes: Component.prototype.types
 	}
 
 	$scope.isConnected = true;
-	$scope.newComponent = {
-		type : $scope.data.componentTypes[0],
-		pin : 13
-	};
+	$scope.newComponent = new Component($scope.data.componentTypes[0]);
 	$scope.components = [];
 
 
 	$scope.addComponent = function() {
 		$scope.components.push($scope.newComponent);
-		$scope.newComponent = {
-			type : $scope.newComponent.type,
-			pin : $scope.newComponent.pin + 1
-		};
+		$scope.newComponent = new Component($scope.data.componentTypes[0]);
 	};
 
 	$scope.toggle = function(component) {
